cc527eedeaa1a8ee7d087328c108f0c33259d2de
JoseLazaroRios <164257967+JoseLazaroRios@users.noreply.github.com>
2025-01-23 10:17:32 -0500

    Segundo avance de script

--- Cupon/script.js
+++ Cupon/script.js
@@ -1,5 +1,46 @@
+function aplicarDescuentoCallback(originalPrice, callback) {
+    setTimeout(() => {
+        try {
+            const discountPrice = originalPrice * 0.80;
+            callback(null, discountPrice);
+        } catch (error) {
+            callback(error);
+        }
+    }, 1000);
+}
+
+
+function aplicarDescuentoPromesa(originalPrice) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                const discountPrice = originalPrice * 0.80;
+                resolve(discountPrice);
+            } catch (error) {
+                reject(error);
+            }
+        }, 1000);
+    });
+}
+
+
 function aplicarDescuento() {
-    var originalPrice = document.getElementById('originalPrice').value;
-    var discountPrice = originalPrice * 0.80; // Aplica el 20% de descuento
-    document.getElementById('finalPrice').innerText = 'Precio Final: $' + discountPrice.toFixed(2);
+    const originalPrice = document.getElementById('originalPrice').value;
+
+
+    aplicarDescuentoCallback(originalPrice, (error, discountPrice) => {
+        if (error) {
+            console.error('Error al aplicar el descuento:', error);
+        } else {
+            document.getElementById('finalPrice').innerText = 'Precio Final (Callback): $' + discountPrice.toFixed(2);
+        }
+    });
+
+    aplicarDescuentoPromesa(originalPrice)
+        .then(discountPrice => {
+            document.getElementById('finalPrice').innerText = 'Precio Final (Promise): $' + discountPrice.toFixed(2);
+        })
+        .catch(error => {
+            console.error('Error al aplicar el descuento:', error);
+        });
 }
